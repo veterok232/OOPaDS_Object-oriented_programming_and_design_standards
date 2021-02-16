64911939abf6dca371df985ff7ba6c4483d642bf
veterok232 <veterok232@yandex.ru>
2021-02-16 21:44:44 +0300

    Первая лаба по ООТПиСП

---
+++ laba1/laba1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace laba1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cDrawField = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // cDrawField
+            // 
+            this.cDrawField.Location = new System.Drawing.Point(0, 0);
+            this.cDrawField.Name = "cDrawField";
+            this.cDrawField.Size = new System.Drawing.Size(800, 600);
+            this.cDrawField.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.cDrawField);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Graphics";
+            this.Shown += new System.EventHandler(this.frmMain_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel cDrawField;
+    }
+}
+
